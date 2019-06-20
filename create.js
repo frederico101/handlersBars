21560058d46561778987358fbfe70a5e46184828
frederico101 <fredyalves.fredyalves@hotmail.com>
2019-06-20 13:19:28 -0300

    handlersBars

--- create.js
+++ create.js
@@ -1,29 +1,34 @@
 
-$(document).ready(function(){
+//$(document).ready(function(){
+   var ourRequest = new XMLHttpRequest();
+   ourRequest.open('GET', 'https://learnwebcode.github.io/json-example/pets-data.json');
+   ourRequest.onload = function(){
+   
+         if(ourRequest.status >= 200 && ourRequest.status < 400){
+                 
+             var data = JSON.parse(ourRequest.responseText);
+             console.log(data);
+             createHTML(data);
+         }else{
+           console.log("We connected, but theres a error");
+         }
 
-
- 
-    $.ajax({
-        url: "https://learnwebcode.github.io/json-example/pets-data.json",
-       // headers: {'Access-Control-Allow-Origin': 'http://127.0.0.1:8000/api/users'},
-       //  url: "http://127.0.0.1:8000/api/users/",
-         method:"GET",
-         dataType : "json",
-       //  jsonpCallback: 'pets',
-        success: function(response){
-            
-             console.log(response);
-             createHTML(response);
-             }
-        }),
-        function createHTML(petsData){
-          var rawTemplate = Document.getElementById("petsTemplate").innerHTML;
-          var compiledTemplate = Handlebars.compile(rawTemplate);
-          var ourGenaretedHTML = compiledTamplete(petsData);
-
-
-            var petsContainer = documentGetElementById("pets-container");
-        }
-
+   };
     
-    });
+     ourRequest.onerror = function(){
+        console.log("Connection error");
+     };
+
+     ourRequest.send();
+ 
+
+     function createHTML(petsData){
+      var rawTemplate = document.getElementById("petsTemplate").innerHTML;
+      var compiledTemplate = Handlebars.compile(rawTemplate);
+      var ourGeneratedHTML = compiledTemplate(petsData);
+
+
+        var petsContainer = document.getElementById("pets-container");
+         petsContainer.innerHTML = ourGeneratedHTML;
+    
+      };
